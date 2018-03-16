91c42b7c7d8a61c9dd30851d2ee2a79cca996291
Thomas Ngo <tngo0508@gmail.com>
2018-03-15 22:52:16 -0700

    Complete the Bronze Challenge: Creating a Custom Error Page

--- chattrbox/index.js
+++ chattrbox/index.js
@@ -3,8 +3,12 @@ var fs = require("fs");
 var extract = require("./extract");
 
 var handleError = function(err, res) {
+  var filePath = extract("/error.html");
+  console.log(filePath);
   res.writeHead(404);
-  res.end();
+  fs.readFile(filePath, function(req, data) {
+    res.end(data);
+  });
 };
 
 var server = http.createServer(function(req, res) {
